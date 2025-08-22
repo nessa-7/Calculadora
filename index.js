6bbd14ad5add6ee9c48e64c7afe4beeccf3cc183
nessa <yisel7948@gmail.com>
2025-08-21 21:49:46 -0500

    actualizacion de rutas a numeros

--- index.js
+++ index.js
@@ -5,22 +5,27 @@ const PORT = 3000
 const a = 12
 const b = 5
 
-app.get('/suma', (req, res) => {
+app.get('/', (req, res) => {
+  res.send("Menu:"+ `<br>` + "/1 : Suma" + `<br>` + " /2 : Resta" + `<br>` + " /3 : Multiplicacion" + `<br>` + " /4 : Division")
+})
+
+
+app.get('/1', (req, res) => {
   const suma = a + b
   res.send(suma)
 })
 
-app.get('/resta', (req, res) => {
+app.get('/2', (req, res) => {
   const resta = a - b
   res.send(resta)
 })
   
-app.get('/multiplicacion', (req, res) => {
+app.get('/3', (req, res) => {
   const multiplicacion = a * b
   res.send(multiplicacion)
 })
 
-app.get('/division', (req, res) => {
+app.get('/4', (req, res) => {
   const division = a / b
   res.send(division)
 })
